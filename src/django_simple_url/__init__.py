2cf764d2cc0dcf4eac966991d69805e929a74c7d
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-09-14 18:57:42 +0200

    Support using qualified regular expressions.
    It is really handy for certain regular expressions to be available by
    default. This way there is no need to worry about what will exactly be
    matched.

--- src/django_simple_url/__init__.py
+++ src/django_simple_url/__init__.py
@@ -2,14 +2,65 @@ import re
 
 from django.conf.urls import url
 
-PARAMETER_REGEX = re.compile(r'\\:(?P<parameter>[A-Za-z0-9_]+)')
+
+def _np(name, regex):
+    '''Return regex named group.'''
+    return '(?P<' + name + '>' + regex + ')'
+
+
+IDENT_REGEX = r'[A-Za-z0-9_]+'
+TYPED_PARAMATER_REGEX = re.compile(''.join([
+    r'\\<',
+    _np('type_name', IDENT_REGEX),
+    r'\\:',
+    _np('parameter', IDENT_REGEX),
+    r'\\>',
+]))
+UNTYPED_PARAMETER_REGEX = re.compile(r'\\:' + _np('parameter', IDENT_REGEX))
+
+
+def _earliest_match(string, *regexes):
+    match = None
+    for regex in regexes:
+        new_match = re.search(regex, string)
+        if not new_match:
+            continue
+        if not match or match.start() > new_match.start():
+            match = new_match
+    return match
 
 
 class URLTranslator(object):
+    def __init__(self):
+        self._mapping_registry = {}
+
+    def register(self, name, regex):
+        self._mapping_registry[name] = regex
+
+    def _translate_part(self, parameter, type_name=None):
+        if type_name is None:
+            return _np(parameter, IDENT_REGEX)
+
+        try:
+            return _np(parameter, self._mapping_registry[type_name])
+        except KeyError:
+            raise ValueError("Unknown regex type: {}".format(type_name))
+
     def translate(self, route):
-        re_route = re.escape(route)
-        re_route = PARAMETER_REGEX.sub(r'(?P<\1>[A-Za-z0-9_]+)', re_route)
-        return re_route
+        unparsed = re.escape(route)
+        chunks = []
+
+        regexes = [TYPED_PARAMATER_REGEX, UNTYPED_PARAMETER_REGEX]
+        while True:
+            match = _earliest_match(unparsed, *regexes)
+            if not match:
+                chunks.append(unparsed)
+                break
+            chunks.append(unparsed[:match.start()])
+            chunks.append(self._translate_part(**match.groupdict()))
+            unparsed = unparsed[match.end():]
+
+        return ''.join(chunks)
 
 
 def simple_url(route, view, *args, **kwargs):
--- src/tests/test_url_translator.py
+++ src/tests/test_url_translator.py
@@ -9,18 +9,25 @@ class URLTranslatorTests(SimpleTestCase):
 
     def test_url_translator_maps_normal_url_to_url(self):
         self.assertEqual(
-            self.translator.translate('/hello/world/'),
-            r'\/hello\/world\/',
+            self.translator.translate('hello/world/'),
+            r'hello\/world\/',
         )
 
     def test_url_translator_escapes_regex_special_characters(self):
         self.assertEqual(
-            self.translator.translate('/hello.world/'),
-            r'\/hello\.world\/'
+            self.translator.translate('hello.world/'),
+            r'hello\.world\/'
         )
 
     def test_url_translator_maps_parameters_to_patterns(self):
         self.assertEqual(
-            self.translator.translate('/:year/'),
-            r'\/(?P<year>[A-Za-z0-9_]+)\/',
+            self.translator.translate(':year/'),
+            r'(?P<year>[A-Za-z0-9_]+)\/',
+        )
+
+    def test_registering(self):
+        self.translator.register('foo_int', r'[0-9]+')
+        self.assertEqual(
+            self.translator.translate('<foo_int:year>/'),
+            r'(?P<year>[0-9]+)\/',
         )
