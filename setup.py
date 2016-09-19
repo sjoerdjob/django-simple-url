5919dd96c89399763aee5f6724110cd4b91942c9
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-09-19 08:30:53 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='django-simple-url',
+    version='0.0.1',
+    description='Simpler URL specification for Django.',
+    author="Sjoerd Job Postmus",
+    author_email='sjoerdjob@sjec.nl',
+    url='https://github.com/sjoerdjob/django-simple-url',
+    packages=['django_simple_url'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    license='MIT license',
+    keywords=['django', 'url'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
